7c1e28618c6b5ae45929b5250faec46a56ece02c
Denis Craciun <51826214+deniscraciungabriel@users.noreply.github.com>
2021-09-04 13:57:12 +0200

    Update getSession.js

--- public/getSession.js
+++ public/getSession.js
@@ -3,6 +3,6 @@
 const localStorageSession = localStorage.getItem("sessionId");
 
 if (!localStorageSession){
-    window.location.replace("https://handmadeandrushka.herokuapp.com");
+    window.location.replace("http://handmadeandrushka.herokuapp.com");
 }
 
